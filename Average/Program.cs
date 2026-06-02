2e2ad4d0683492e407b6d2e28c590d3cb31133d0
Xavier Casilla <Casillaxavier13@gmail.com>
2026-06-02 07:18:21 -0400

    Práctica: cálculo del promedio de un conjunto de valores numéricos

---
+++ Average/Program.cs
@@ -0,0 +1,11 @@
+﻿using Average.Servives;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CalcularPromedio promedioService = new CalcularPromedio();
+
+        promedioService.Calcular();
+    }
+}
---
+++ Average/Servives/CalcularPromedio.cs
@@ -0,0 +1,29 @@
+﻿namespace Average.Servives
+{
+    public class CalcularPromedio
+    {
+        public void Calcular()
+        {
+            int suma = 0;
+            int cantidad = 0;
+            int input;
+
+            do
+            {
+                Console.Write("Digite un número (0 para terminar): ");
+                input = Convert.ToInt32(Console.ReadLine());
+
+                if (input != 0)
+                {
+                    suma += input;
+                    cantidad++;
+                }
+
+            } while (input != 0);
+
+            double promedio = (double)suma / cantidad;
+
+            Console.WriteLine($"Promedio: {promedio:F2}");
+        }
+    }
+}
